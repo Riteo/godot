1576e59e35f0b48e774edda6bacba9351f26571a
Riteo Siuga <lorenzocerqua@tutanota.com>
2021-04-11 17:05:18 +0200

    Revert "Focus all windows only on mouse clicks"
    This reverts commit 091be1783ab39a3ea4a80413898aea52d4ab4453, as it was the wrong approach altogheter.

--- scene/main/window.cpp
+++ scene/main/window.cpp
@@ -893,18 +893,15 @@ void Window::_window_input(const Ref<InputEvent> &p_ev) {
 	}
 
 	if (exclusive_child != nullptr) {
-		Ref<InputEventMouseButton> b = p_ev;
-		if (b.is_valid() && b->is_pressed() && b->get_button_index() == 0) {
-			Window *focus_target = exclusive_child;
+		Window *focus_target = exclusive_child;
+		focus_target->grab_focus();
+		while (focus_target->exclusive_child != nullptr) {
+			focus_target = focus_target->exclusive_child;
 			focus_target->grab_focus();
-			while (focus_target->exclusive_child != nullptr) {
-				focus_target = focus_target->exclusive_child;
-				focus_target->grab_focus();
-			}
+		}
 
-			if (!is_embedding_subwindows()) { //not embedding, no need for event
-				return;
-			}
+		if (!is_embedding_subwindows()) { //not embedding, no need for event
+			return;
 		}
 	}
 
